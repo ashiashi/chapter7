6110be55a16ae82d2ccbcd853f8664ba33431bd1
ashiashi <358572188@qq.com>
2018-07-14 10:51:27 +0800

    Delete getip.py

--- getip.py
+++
@@ -1,7 +0,0 @@
-import urllib2
-import re
-def run(**args):
-    url = urllib2.urlopen("http://txt.go.sohu.com/ip/soip")
-    text = url.read()
-    ip = re.findall(r'\d+.\d+.\d+.\d+',text)
-    return ip[0]
