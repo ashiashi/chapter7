10d3fb87ea0467b788455e6045688f0cbe23d137
ashiashi <358572188@qq.com>
2018-07-15 11:52:31 +0800

    Update auto.bat

--- download/auto.bat
+++ download/auto.bat
@@ -1,3 +1,2 @@
-path:C:\Users\name\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
--set ws=WScript.CreateObject(“WScript.Shell”)
--ws.Run “D:\abc.bat /start”,0
+set ws=WScript.CreateObject(“WScript.Shell”)
+ws.Run “D:\abc.bat /start”,0
