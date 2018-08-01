a0aada87dfb553c299ebf99636a0fbd8d0ba32ee
ashiashi <358572188@qq.com>
2018-08-01 20:58:23 +0800

    change

--- download/auto.bat
+++ download/auto.bat
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 ﻿@echo off
 start  "" "C:\Windows\System32\calc.exe"
-=======
-@echo off
-start  "" "C:\Windows\System32\calc.exe"
->>>>>>> fff840fdb7e107f43d6b2e2e5c87da3eaf237996
-=======
-@echo off
-start  "" "C:\Windows\System32\calc.exe"
->>>>>>> fff840fdb7e107f43d6b2e2e5c87da3eaf237996
+
